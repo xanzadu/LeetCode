6651d048f5454666a9780226de37f404f2ddee10
Noah J Lehman <noahjlehman@gmail.com>
2022-01-11 14:29:05 -0800

    Time: 207 ms (17.71%), Space: 52.2 MB (24.00%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -4,15 +4,17 @@
  */
 var productExceptSelf = function(nums) {
     const result = [];
-    let productSoFar = 1;
+    let currentProd = 1;
+    
     for (let i = 0; i < nums.length; i++) {
-        result[i] = productSoFar
-        productSoFar *= nums[i]
+        result[i] = currentProd;
+        currentProd *= nums[i];
     }
-    productSoFar = 1
+    currentProd = 1;
     for (let j = nums.length-1; j >= 0; j--) {
-        result[j] *= productSoFar
-        productSoFar *= nums[j]
+        result[j] *= currentProd;
+        currentProd *= nums[j];
     }
     return result;
+    
 };
