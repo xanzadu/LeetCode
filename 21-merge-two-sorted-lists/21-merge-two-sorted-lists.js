f99422e748fce895e1a56f20191ed5e7887358dc
Noah J Lehman <noahjlehman@gmail.com>
2022-02-07 17:53:07 -0800

    Time: 144 ms (14.59%), Space: 44.5 MB (8.25%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,31 +11,14 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    let list = new ListNode()
-    let head = list
+    if (l1 === null) return l2;
+    if (l2 === null) return l1;
     
-    while (l1 !== null && l2 !== null) {
-	
-		// Select the smallest value from either linked list,
-		// then increment that list forward.
-        if (l1.val < l2.val) {
-            list.next = new ListNode(l1.val)
-            l1 = l1.next
-        } else {
-            list.next = new ListNode(l2.val)
-            l2 = l2.next
-        }
-        
-        list = list.next
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
     }
-    
-	// It's possible that one linked list is shorter than the other so we just
-	// add on the remainder of the last linked list. It's already sorted :)
-    if (l1 !== null)
-        list.next = l1
-    if (l2 !== null)
-        list.next = l2
-    
-	// return .next because this first element in the linkedlist is empty
-    return head.next
 };
